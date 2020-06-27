27328e558f7189c3f0269dce019b8efa3c5211a1
lumin <834964182@qq.com>
2020-06-27 18:49:57 +0800

    完成了登录注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,8 @@ $(function () {
         $('.reg-box').hide();
     })
 
+    const form = layui.form;
+    const layer = layui.layer;
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -20,10 +22,52 @@ $(function () {
             // 还需要拿到密码框中的内容
             // 然后进行一次等于的判断
             // 如果判断失败,则return一个提示消息即可
-            var pwd = $('.reg-box [name=password]').val()
+            const pwd = $('.reg-box [name=password]').val()
+            // const pwd = $('#password').val();
             if (pwd !== value) {
                 return '两次密码不一致！'
             }
         }
     })
+
+    // 注册 ajax请求
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        const inputParams = {
+            username: $('#form_reg [name="username"]').val(),
+            password: $('#form_reg [name="password"]').val()
+        }
+        $.post('/api/reguser', inputParams, function (res) {
+            if (res.status !== 0) {
+                
+                return layer.msg(res.message)
+            }
+            
+            layer.msg('注册成功,请登录')
+            $('#link_login').click();
+        })
+    })
+    // 登录 ajax请求
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: `/api/login`,
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    // return console.log('登录失败');
+                    return layer.msg('登录失败')
+                }
+                // console.log('登录成功');
+                layer.msg('登录成功')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '../../index.html'
+            }
+        })
+    })
+
+
 })
